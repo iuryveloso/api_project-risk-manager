1ae1edd616d79beec0ff986da8c2c68ee49c0fed
Iury Veloso <iurysveloso@gmail.com>
2022-12-20 00:57:05 -0300

    Save and delete risktasks by task hierarchy

--- src/controllers/riskTaskController.ts
+++ src/controllers/riskTaskController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import RiskTask from '@models/RiskTask'
 import { RiskTaskRequest } from '@interfaces/riskTaskInterfaces'
+import Task from '@models/Task'
 
 class RiskTaskController {
   public async list (req: RiskTaskRequest, res: Response) {
@@ -32,9 +33,49 @@ class RiskTaskController {
       riskID,
       taskID
     })
+    const task = await Task.findById(taskID)
+    let saveCounter: number = 0
     try {
+      async function saveChildTasks (taskID: string) {
+        const childTasks = await Task.find({ parentTaskID: taskID })
+        childTasks.forEach(async (childTask) => {
+          if (childTask._id) {
+            const childTaskId = childTask._id.toString()
+            const isChildTasksaved = riskTasks.filter((riskTask) => {
+              return riskTask.taskID?.includes(childTaskId) && riskTask.riskID?.includes(riskID)
+            })
+            if (isChildTasksaved.length === 0) {
+              saveCounter++
+              await RiskTask.create({ riskID, taskID: childTaskId })
+              await saveChildTasks(childTaskId)
+            }
+          }
+        })
+      }
+      async function saveParentTasks (taskID: string) {
+        const parentTask = await Task.findById(taskID)
+        const siblingsTasks = await Task.find({ parentTaskID: taskID })
+        const siblingsTasksSaved = siblingsTasks.filter((siblingTask) => {
+          if (siblingTask._id) {
+            return riskTasks.filter((riskTask) => {
+              return riskTask.taskID?.includes(siblingTask._id.toString()) && riskTask.riskID?.includes(riskID)
+            }).length > 0
+          } else return false
+        })
+        console.log({ a: siblingsTasks.length, b: siblingsTasksSaved.length })
+        if (siblingsTasks.length === siblingsTasksSaved.length) {
+          saveCounter++
+          await RiskTask.create({ taskID, riskID })
+          await saveParentTasks(parentTask?.parentTaskID as string)
+        }
+      }
       await RiskTask.create(riskTask)
-      return res.status(201).json({ message: 'Tarefa adicionada com sucesso!' })
+
+      const riskTasks = await RiskTask.find()
+      await saveChildTasks(taskID)
+      await saveParentTasks(task?.parentTaskID as string)
+
+      return res.status(201).json({ message: saveCounter > 0 ? 'Tarefas adicionadas com sucesso!' : 'Tarefa adicionada com sucesso!' })
     } catch (error) {
       console.log(error)
       return res.status(500).json({ message: 'Aconteceu algum erro, tente novamente mais tarde!' })
@@ -44,6 +85,8 @@ class RiskTaskController {
   public async delete (req: Request, res: Response) {
     const riskID = req.params.riskID
     const taskID = req.params.taskID
+    const task = await Task.findById(taskID)
+    let deleteCounter: number = 0
     try {
       const riskTask = await RiskTask.find({ riskID, taskID })
 
@@ -51,9 +94,42 @@ class RiskTaskController {
         return res.status(422).json({ message: 'Risco não encontrado!' })
       }
 
-      await RiskTask.deleteMany({ riskID, taskID })
+      const riskTasks = await RiskTask.find()
+      async function deleteChildTasks (taskID: string) {
+        const childTasks = await Task.find({ parentTaskID: taskID })
+        childTasks.forEach(async (childTask) => {
+          if (childTask._id) {
+            const childTaskId = childTask._id.toString()
+            const isChildTasksaved = riskTasks.filter((riskTask) => {
+              return riskTask.taskID?.includes(childTaskId) && riskTask.riskID?.includes(riskID)
+            })
+            if (isChildTasksaved.length > 0) {
+              deleteCounter++
+              await RiskTask.deleteMany({ riskID, taskID: childTaskId })
+              await deleteChildTasks(childTaskId)
+            }
+          }
+        })
+      }
 
-      return res.status(200).json({ message: 'Tarefa removida com sucesso!' })
+      async function deleteParentTasks (taskID: string) {
+        const parentTask = await Task.findById(taskID)
+        const isParentTaskSaved = riskTasks.filter((riskTask) => {
+          if (parentTask?._id) {
+            return riskTask.taskID?.includes(parentTask?._id.toString()) && riskTask.riskID?.includes(riskID)
+          } else return false
+        }).length > 0
+        if (isParentTaskSaved) {
+          deleteCounter++
+          await RiskTask.deleteMany({ taskID: parentTask?._id.toString(), riskID })
+          await deleteParentTasks(parentTask?.parentTaskID as string)
+        }
+      }
+
+      await RiskTask.deleteMany({ riskID, taskID })
+      await deleteChildTasks(taskID)
+      await deleteParentTasks(task?.parentTaskID as string)
+      return res.status(200).json({ message: deleteCounter > 0 ? 'Tarefas removidas com sucesso!' : 'Tarefa removida com sucesso!' })
     } catch (error) {
       console.log(error)
       return res.status(500).json({ message: 'Aconteceu algum erro, tente novamente mais tarde!' })
